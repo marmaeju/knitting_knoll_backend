187570463616962a37534da4ce6bf748d1ba13bb
Marley Judd <marleymaejudd@gmail.com>
2022-12-09 13:50:01 -0700

    CreateStitch made

--- controllers/StitchController.js
+++ controllers/StitchController.js
@@ -1,3 +1,63 @@
-const { Stitches } = require('../models')
+const { Stitch } = require('../models')
 
-module.exports = {}
+//Create
+const CreateStitch = async (req, res) => {
+  try {
+    const stitchContent = {
+      ...req.body
+    }
+    let stitch = await Stitch.create(stitchContent)
+    res.send(stitch)
+  } catch (error) {
+    throw error
+  }
+}
+
+//Read All
+const GetAllCasts = async (req, res) => {
+  try {
+    const casts = await Cast.findAll()
+    res.send(casts)
+  } catch (error) {
+    throw error
+  }
+}
+
+//Read One
+const GetOneCast = async (req, res) => {
+  try {
+    const cast = await Cast.findByPk(req.params.cast_id)
+    res.send(cast)
+  } catch (error) {
+    throw error
+  }
+}
+
+//Update
+const UpdateCast = async (req, res) => {
+  try {
+    let cast_id = parseInt(req.params.cast_id)
+    let updatedCast = await Cast.update(req.body, {
+      where: { id: cast_id },
+      returning: true
+    })
+    res.send(updatedCast)
+  } catch (error) {
+    throw error
+  }
+}
+
+//Delete
+const DeleteCast = async (req, res) => {
+  try {
+    let cast_id = parseInt(req.params.cast_id)
+    await Cast.destroy({ where: { id: cast_id } })
+    res.send({ message: `Deleted material with an id of ${cast_id}` })
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  CreateStitch
+}
--- routes/StitchRouter.js
+++ routes/StitchRouter.js
@@ -1,4 +1,10 @@
 const router = require('express').Router()
 const controller = require('../controllers/StitchController')
 
+router.post('/', controller.CreateStitch)
+// router.get('/', controller.GetAllCasts)
+// router.get('/:cast_id', controller.GetOneCast)
+// router.put('/:cast_id', controller.UpdateCast)
+// router.delete('/:cast_id', controller.DeleteCast)
+
 module.exports = router

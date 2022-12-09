d221cba60b7c4749cdc957823a9163f5d187dc76
Marley Judd <marleymaejudd@gmail.com>
2022-12-09 07:54:23 -0700

    added routes into server.js and updated routes themselves in routes folder

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const cors = require('cors')
+const CastRouter = require('./routes/CastRouter')
+const StitchRouter = require('./routes/StitchRouter')
+const TutorialRouter = require('./routes/TutorialRouter')
 
 const app = express()
-
-// const AppRouter = require('./routes/AppRouter')
 
 const PORT = process.env.PORT || 3001
 
@@ -11,6 +12,9 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use('/cast', CastRouter)
+app.use('/stitch', StitchRouter)
+app.use('/tutorial', TutorialRouter)
+
 app.get('/', (req, res) => res.json({ message: 'Server Works' }))
-// app.use('/api', AppRouter)
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))

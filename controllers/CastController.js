bb6ce066b96cc8d86698a216d04ba3e05b59b8db
Marley Judd <marleymaejudd@gmail.com>
2022-12-09 13:17:48 -0700

    CreateCast created in controller and route and working in insomnia

--- controllers/CastController.js
+++ controllers/CastController.js
@@ -1,3 +1,63 @@
-const { Casts } = require('../models')
+const { Cast } = require('../models')
 
-module.exports = {}
+//Create
+const CreateCast = async (req, res) => {
+  try {
+    const castContent = {
+      ...req.body
+    }
+    let cast = await Cast.create(castContent)
+    res.send(cast)
+  } catch (error) {
+    throw error
+  }
+}
+
+//Read All
+const GetAllMaterials = async (req, res) => {
+  try {
+    const materials = await Material.findAll()
+    res.send(materials)
+  } catch (error) {
+    throw error
+  }
+}
+
+//Read One
+const GetOneMaterial = async (req, res) => {
+  try {
+    const material = await Material.findByPk(req.params.material_id)
+    res.send(material)
+  } catch (error) {
+    throw error
+  }
+}
+
+//Update
+const UpdateMaterial = async (req, res) => {
+  try {
+    let material_id = parseInt(req.params.material_id)
+    let updatedMaterial = await Material.update(req.body, {
+      where: { id: material_id },
+      returning: true
+    })
+    res.send(updatedMaterial)
+  } catch (error) {
+    throw error
+  }
+}
+
+//Delete
+const DeleteMaterial = async (req, res) => {
+  try {
+    let material_id = parseInt(req.params.material_id)
+    await Material.destroy({ where: { id: material_id } })
+    res.send({ message: `Deleted material with an id of ${material_id}` })
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  CreateCast
+}
--- routes/CastRouter.js
+++ routes/CastRouter.js
@@ -1,4 +1,10 @@
 const router = require('express').Router()
 const controller = require('../controllers/CastController')
 
+router.post('/', controller.CreateCast)
+// router.get('/', controller.GetAllMaterials)
+// router.get('/:material_id', controller.GetOneMaterial)
+// router.put('/:material_id', controller.UpdateMaterial)
+// router.delete('/:material_id', controller.DeleteMaterial)
+
 module.exports = router
